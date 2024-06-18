b42a84dd2da3aa7c60f929cc17dc759752c13821
ramrachai <ramrachaim@gmail.com>
2024-06-18 22:08:51 +0600

    disabled image form and list component

--- frontend/app/image/page.tsx
+++ frontend/app/image/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ImageList from './ImageList';
-import UploadForm from './UploadForm';
+// import ImageList from './ImageList';
+// import UploadForm from './UploadForm';
 
 const imagePage = () => {
     return (
         <div className=" flex justify-center items-center flex-col md:items-start  md:flex-row gap-3 max-w-5xl m-auto p-6">
-            <UploadForm />
-            <ImageList />
+            <h2>Hello Image gallery page</h2>
+            {/* <UploadForm /> */}
+            {/* <ImageList /> */}
         </div>
     );
 };
